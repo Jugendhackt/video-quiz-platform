62d159edf8e9575ea34375adb4ed2af639b10caf
Julie Steinbach <julie.steinbach@hacker-school.de>
2021-05-15 18:05:34 +0200

    Position of some Buttons changed

--- pages/list.jsx
+++ pages/list.jsx
@@ -7,12 +7,12 @@ export default function ListPage() {
             <div>
             <iframe class="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
-            <div style={{textAlign: "right"}}className="Quiz-Button">
+            <div style={{textAlign: "right"}} className="Quiz-Button">
                 <Button variant="primary">Quiz</Button>
             </div>
             <br></br>
-            <div style={{textAlign: "left"}}className="Back-Button">
-                <><Button variant="primary">Back</Button></>
+            <div style={{textAlign: "left"}} className="Back-Button">
+                <Button variant="primary">Back</Button>
             </div>
         </div>
         
