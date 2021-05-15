07d8c0493441e98936c7a3f87a25dddb6fc38f2a
Julie Steinbach <julie.steinbach@hacker-school.de>
2021-05-15 17:01:22 +0200

    Video Border Changed

--- pages/list.jsx
+++ pages/list.jsx
@@ -5,7 +5,9 @@ export default function ListPage() {
     return (
         <div>
             <Button variant="primary">Primary</Button>
+            <div>
             <iframe class="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
         </div>
         
     )
