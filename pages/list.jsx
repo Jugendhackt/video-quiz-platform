8aa2dab2c7baa48c9fc5ce6ad6d2db98f5028d06
Julie Steinbach <julie.steinbach@hacker-school.de>
2021-05-15 17:21:16 +0200

    Buttons Changed

--- pages/list.jsx
+++ pages/list.jsx
@@ -4,9 +4,15 @@ import { Button } from 'react-bootstrap'
 export default function ListPage() {
     return (
         <div>
-            <Button variant="primary">Primary</Button>
             <div>
             <iframe class="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+            <div className="Quiz-Button">
+                <Button variant="primary">Quiz</Button>
+            </div>
+            <br></br>
+            <div className="Back-Button">
+                <><Button variant="primary">Back</Button></>
             </div>
         </div>
         
