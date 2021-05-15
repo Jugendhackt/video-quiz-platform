cfcbc5db8b3cd5534b18a46f7f7912cf62366bce
Tim-pqy <stibane.tim.pqy@gmail.com>
2021-05-15 21:20:13 +0200

    checkboxen

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -15,18 +15,18 @@ export default function ChatPage() {
                             paddingTop: 25,
                             height: 0
                         }}
-                    >
-                        <iframe
-                            style={{
-                                position: "absolute",
-                                top: 0,
-                                left: 0,
-                                width: "100%",
-                                height: "100%",
-                                border: 0
-                            }}
-                            src="https://www.youtube-nocookie.com/embed/MsrrKMzLeWE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={false}></iframe>
-                    </div>
+                    ></div>
+                    <form><input type="radio" name="Antwort" id="Antwort 1"></input>
+                        <label for="Antwort 1">etwas</label>
+                        <br />
+                        <input type="radio" name="Antwort" id="Antwort 2"></input>
+                        <label for="Antwort 2">anderes</label>
+                        <br />
+                        <input type="radio" name="Antwort" id="Antwort 3"></input>
+                        <label for="Antwort 3">nochwasanderes</label>
+                        <br />
+                        <input type="submit" value="senden"></input>
+                    </form>
                     <br />
                     <ChooseRoom />
                     <ButtonToolbar className="justify-content-between" aria-label="Toolbar with Button groups">
