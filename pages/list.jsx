8a4ba2fb7a72e8dd05b4e74698764c73714dd9fd
Julie Steinbach <julie.steinbach@hacker-school.de>
2021-05-15 16:30:22 +0200

    Noch ein Test

--- pages/list.jsx
+++ pages/list.jsx
@@ -5,7 +5,7 @@ export default function ListPage() {
     return (
         <div>
             <Button variant="primary">Primary</Button>
-            <iframe class="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
         
     )
