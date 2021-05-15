c458a76b689d073e37d4e3600ffb09f68523e6f5
Julie Steinbach <julie.steinbach@hacker-school.de>
2021-05-15 19:09:37 +0200

    Container and Video placed

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, ButtonToolbar, Col, Container, Row } from 'react-bootstrap'
 
 export default function ChatPage() {
     return (
-        <div>
-            <div>
-            <iframe className="video" src="https://www.youtube.com/embed/MsrrKMzLeWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-            <div style={{textAlign: "right"}} className="Quiz-Button">
-                <Button variant="primary">Quiz</Button>
-            </div>
-            <br />
-            <div style={{textAlign: "left"}} className="Back-Button">
-                <Button variant="primary">Back</Button>
-            </div>
-        </div>
-        
+        <Container fluid={true}>
+            <Row>
+                <Col md="6">
+                    <div
+                        style={{
+                            position: "relative",
+                            paddingBottom: "56.25%" /* 16:9 */,
+                            paddingTop: 25,
+                            height: 0
+                        }}
+                    >
+                        <iframe
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                width: "100%",
+                                height: "100%",
+                                border: 0
+                            }}
+                            src="https://www.youtube-nocookie.com/embed/MsrrKMzLeWE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={false}></iframe>
+                    </div>
+                    <br />
+                    <ButtonToolbar className="justify-content-between" aria-label="Toolbar with Button groups">
+                        <Button variant="primary" onClick={() => console.log("hallo welt")}>Back</Button>
+                        <Button variant="primary" onClick={() => console.log("test")}>Quiz</Button>
+                    </ButtonToolbar>
+                </Col>
+                <Col md="6">
+                    CHAT
+                </Col>
+            </Row>
+        </Container>
+
     )
 }
