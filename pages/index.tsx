06b86de2ff3dd2b5b58ad354d300b5654ee3710b
CodeDoctor <wusel905@gmail.com>
2021-05-15 09:17:51 +0000

    add material ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Button from '@material-ui/core/Button';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
+    <Button variant="contained">Default</Button>
       <Link href="/about">
         <a>About</a>
       </Link>
