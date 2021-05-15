537e2d00696d6f53587ae85ebc7164f9afe144e8
CodeDoctorDE <wusel905@gmail.com>
2021-05-15 14:46:26 +0200

    remove material button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import Button from '@material-ui/core/Button';
+import { Button } from 'react-bootstrap'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-    <Button variant="contained">Default</Button>
       <Link href="/about">
         <a>About</a>
       </Link>
